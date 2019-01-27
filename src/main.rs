6ffb6f2058480dbee2cffc8b1ab4df5cfac52d3b
metmirr <metin7087@gmail.com>
2019-01-27 21:06:25 +0300

    Added hash function

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
+extern crate crypto;
 
 use std::time::{SystemTime, UNIX_EPOCH, Duration};
+use crypto::sha2::Sha256;
+use crypto::digest::Digest;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 struct Transaction {
@@ -48,6 +51,13 @@ impl Blockchain {
         let tx = Transaction { sender: sender, recipient: recipient, amount: amount };
         self.current_transactions.push(tx);
     }
+    fn hash(block: &str) -> String{
+        // Creates a SHA-256 hash of a Block
+        let ser = serde_json::to_string(&block).unwrap();
+        let mut hasher = Sha256::new();
+        hasher.input_str(ser.as_str());
+        hasher.result_str()
+    }
 }
 
 fn main() {
@@ -57,6 +67,8 @@ fn main() {
     };
     
     blockchain.new_transaction(String::from("coni"), String::from("ceni"), 152);
+    blockchain.new_transaction(String::from("AA"), String::from("BB"), 44);
+    blockchain.new_transaction(String::from("CC"), String::from("DD"), 94);
     blockchain.new_block(987654321, String::from("previous hash - 1"));
 
     blockchain.new_transaction(String::from("mdemir"), String::from("ali"), 22);
@@ -64,4 +76,6 @@ fn main() {
 
     let ser = serde_json::to_string(&blockchain).unwrap();
     println!("{}", ser);
+
+    println!("{:?}", Blockchain::hash(ser.as_str()));
 }
